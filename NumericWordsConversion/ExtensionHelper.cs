0aa671004d771fb8df27ce0e06c2e51169e28154
samyeak <samyeakmhj@protonmail.com>
2020-02-01 10:36:21 +0545

    Renamed to NumericWordsConversion

---
+++ NumericWordsConversion/ExtensionHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace NumericWordsConversion
+{
+    public static class ExtensionHelper
+    {
+        public static string ToNumericWords(this decimal amount)
+        {
+            return GlobalOptions.NumericWordsConverter.ToWords(amount);
+        }
+
+        public static string ToCurrencyWords(this decimal amount)
+        {
+            return GlobalOptions.CurrencyWordsConverter.ToWords(amount);
+        }
+
+    }
+}
